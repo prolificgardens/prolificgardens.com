ad457bb3a6f978bf64cd57bfa6c19bab71b90448
Justin Mandzik <justin@mandzik.org>
2014-12-11 11:54:18 -0500

    src/dist setup, gulpfile init, gitignore

--- gulpfile.js
+++ gulpfile.js
@@ -2,18 +2,40 @@ var gulp = require('gulp');
 var minifyCSS = require('gulp-minify-css');
 var concat = require('gulp-concat');
 var clean = require('gulp-clean');
+var runSequence = require('run-sequence');
+var uglify = require('gulp-uglify');
 
-gulp.task('default', ['css']);
+var BUILD_DIRECTORY = 'dist/';
 
-gulp.task('default', function () {
-  return gulp.src('dist/', {read: false})
+gulp.task('default', function build(cb) {
+  runSequence('clean', ['styles', 'scripts'], 'html', 'watch', cb);
+});
+
+gulp.task('build', function build(cb) {
+  runSequence('clean', ['styles', 'scripts'], 'html', cb);
+});
+
+gulp.task('clean', function () {
+  return gulp.src(BUILD_DIRECTORY, {read: false})
       .pipe(clean());
 });
 
-gulp.task('css', function () {
+gulp.task('styles', function () {
   gulp.src('src/css/*.css')
       .pipe(minifyCSS())
       .pipe(concat('pg.css'))
-      .pipe(gulp.dest('dist/'))
+      .pipe(gulp.dest(BUILD_DIRECTORY))
+});
+
+gulp.task('scripts', function () {
+  gulp.src('src/js/**/*.js')
+      .pipe(uglify())
+      .pipe(concat('pg.js'))
+      .pipe(gulp.dest(BUILD_DIRECTORY))
+});
+
+gulp.task('html', function () {
+  gulp.src('src/index.html')
+      .pipe(gulp.dest(BUILD_DIRECTORY))
 });
 
